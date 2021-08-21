3220193f93bb0fb3e3fc519e667cadeb89ae572b
michael peterson <michaeljoepeterson@gmail.com>
2021-08-21 16:55:09 -0600

    initial create field value endpoint and field value data struct

--- models/p-file.js
+++ models/p-file.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
 const pFileSchema = mongoose.Schema({
+    patientId:{type:String,required:true, unique:true},
     name:{type:String,required:true},
     ancestors:{type:Array,default:[]},
     parent:{type:String,default:null},
-    sortOrder:{type:Number,default:0}
+    sortOrder:{type:Number,default:0},
+    activityLevel:{type:String,default:null},
+    height:{type:Number,default:null},
+    weight:{type:Number,default:null},
+    heightString:{type:String,default:null},
+    weightString:{type:String,default:null}
 });
 
 pFileSchema.methods.serialize = function(){
@@ -13,7 +19,11 @@ pFileSchema.methods.serialize = function(){
         id:this._id,
         ancestors:this.ancestors,
         parent:this.parent,
-        sortOrder:this.sortOrder
+        sortOrder:this.sortOrder,
+        height:this.height,
+        weight:this.weight,
+        heightString:this.heightString,
+        weightString:this.weightString
 	};
 }
 
